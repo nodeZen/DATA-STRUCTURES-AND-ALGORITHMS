74465985eb3bd2ac405412ec4f838a7a19526d36
nodeZen <suryareddy409@gmail.com>
2024-02-12 21:37:43 +0530

    hash table exercise

--- hash-table-exercise-1.js
+++ hash-table-exercise-1.js
@@ -1,1 +1,13 @@
-// 
+// Findint the first recurrence of an array
+function recurrenceElement(array){
+    const map = {};
+    for(let i = 0 ; i< array.length ; i++){
+        if(map[array[i]])
+            return array[i];
+        else
+        map[array[i]] = i;
+    }
+    return null;
+}
+
+console.log(recurrenceElement([1,2,3,4,4,2,4,1]));
